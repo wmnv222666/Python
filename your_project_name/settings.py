58eef7f20aa7ddb49f8b5cf2a0ebb3183fecb9a6
wei <alex20180614@gmail.com>
2024-02-28 11:50:56 -0500

    updata

--- your_project_name/settings.py
+++ your_project_name/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app01.apps.App01Config",
     "project1.apps.Project1Config"
 ]
 
